cd4ca0ba1b6e9930e4a13e32a3e53b36faa0f391
Juan <44310630+jrosario5@users.noreply.github.com>
2023-02-26 17:29:17 -0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,6 +1,26 @@
 import spacy
+import re
+import os
+from datetime import datetime
+from tika import parser
+import json
+class Parser:
+    def __init__(self, document):
+        self.document = document
+        self.parsed = parser.from_file(self.document)
+    def process(self):
+        text = self.parsed['content']
+        if text is None:
+            return 'No extractable text'
+        doc = nlp(text)
+        data = {"props":[], 'values':[]}
+        for val in doc.ents:
+            data['props'].append(val.label_)
+            data['values'].append(val.text)
+        return (json.dumps(data))
 
-nlp = spacy.load('GeneralResume_Parse_SMALL_v1')
-
-def get_name(doc):
-    pass
+if __name__ == '__main__':
+    nlp = spacy.load("linkedinparser")
+    parser = Parser("TaylorFosterResume.pdf")
+    resume = parser.process()
+    print(resume)
